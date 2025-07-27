a0e3c3ff168b309353297b8c8ba2f26639de48f4
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-27 21:44:59 +0500

    Version 0.12

--- 09-js-Functions/functions.js
+++ 09-js-Functions/functions.js
@@ -1,18 +1,35 @@
 let age = 21;
-let adultYears = age - 18;
-alert(adultYears)
+let adultYears;
 
-age = 40;
-adultYears = age - 18;
-alert(adultYears)
+// age = 40;
+// adultYears = age - 18;
+// alert(adultYears)
 
-age = 45;
-adultYears = age - 18;
-alert(adultYears)
+// age = 45;
+// adultYears = age - 18;
+// alert(adultYears)
 
-age = 55;
-adultYears = age - 18;
-alert(adultYears)
+// age = 55;
+// adultYears = age - 18;
+// alert(adultYears)
 
 // You can clearly see that we have duplication in our code 
 // To tackle down this duplication we have "Functions" in javascript
+
+function calculateAdultYears() {
+    adultYears = age - 18;
+}
+
+calculateAdultYears()
+alert(adultYears)
+
+age = 45;
+calculateAdultYears()
+alert(adultYears)
+
+age = 55;
+calculateAdultYears()
+alert(adultYears)
+
+// Now we don't have to do the calculations every time for adult years
+// and it is due to the functions
