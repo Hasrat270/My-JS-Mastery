d0ac91453b503680b46400bb5d8c97b7e099ee7b
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-27 21:21:41 +0500

    Version 0.10

---
+++ 08-js-Objects/objects.js
@@ -0,0 +1,26 @@
+// JavaScript Objects Examples
+let profile = {
+    userName: "Hasrat Afridi",
+    email: "hasrat@email.com",
+    age: "21",
+    city: "Karachi",
+    salary: "50000$"
+}
+
+// Playing around with JavaScript Variables
+
+let age = 21;
+let adultYears = age - 18;
+alert(adultYears)
+
+age = 40;
+adultYears = age - 18;
+alert(adultYears)
+
+age = 45;
+adultYears = age - 18;
+alert(adultYears)
+
+age = 55;
+adultYears = age - 18;
+alert(adultYears)
