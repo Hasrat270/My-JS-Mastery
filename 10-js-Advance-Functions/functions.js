773349109e7972de3bae9e612f47327103365794
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-28 01:29:18 +0500

    Version 0.14

--- 10-js-Advance-Functions/functions.js
+++ 10-js-Advance-Functions/functions.js
@@ -1,23 +1,36 @@
 // Passing data into functions with parameters
 
-let age = 21;
-let totalAdultYears;
+let age = 21; // Defining a variable to hold the age of the user
+let totalAdultYears; // Variable to hold the total adult years calculated
 
 function calculateAdultYears(userAge) {
-    return userAge - 18;
+    let result; // using a local variable to store the result
+    result = userAge;  // Assigning the value of userAge to result
+    return result - 18; // Subtracting 18 from the result to calculate adult years
 }
 
-totalAdultYears = calculateAdultYears(age)
-alert(totalAdultYears)
+totalAdultYears = calculateAdultYears(age) // Calling the function with age as an argument
+alert(totalAdultYears) // Displaying the result in an alert
 
-age = 45;
-totalAdultYears = calculateAdultYears(age)
-alert(totalAdultYears)
-
-age = 55;
-totalAdultYears =  calculateAdultYears(age)
-alert(totalAdultYears)
+age = 45; // Changing the age variable to a new value
+totalAdultYears = calculateAdultYears(age) // Calling the function again with the new age
+alert(totalAdultYears) // Displaying the new result in an alert
+ 
+age = 55; // Changing the age variable again
+totalAdultYears =  calculateAdultYears(age) // Calling the function again with the new age
+alert(totalAdultYears) // Displaying the new result in an alert
 
 // Now we don't have to do the calculations every time for adult years
 // and it is due to the functions
 // In the previous example, we had to calculate adult years multiple times
+
+
+let salary = 2000; // Defining a variable to hold the salary of the user
+let expenses = 100; // Defining a variable to hold the expenses of the user
+let savings; // Variable to hold the savings calculated
+
+function calculateSavings(salary, expenses) {
+    savings = salary - expenses; // Calculating savings by subtracting expenses from salary
+}
+calculateSavings(salary, expenses); // Calling the function with salary and expenses as arguments
+alert(savings); // Displaying the savings in an alert
