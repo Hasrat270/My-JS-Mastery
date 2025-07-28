b45b0ad5be0b09b851c8798d5b1dbf5d505c7aef
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-29 01:03:21 +0500

    Version 0.19

---
+++ 12-js-Methods/methods.js
@@ -0,0 +1,11 @@
+// Methods in JavaScript
+
+let person = {
+    name: "Hasrat",
+    age: 25,
+    greet() {
+        alert(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
+    }
+};
+
+person.greet(); // Calling the method to greet
