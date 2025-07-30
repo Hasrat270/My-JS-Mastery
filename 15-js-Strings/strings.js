ad92e638de98939da60d566e10d6989b4c2e344f
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-30 12:00:26 +0500

    Version 0.26

--- 15-js-Strings/strings.js
+++ 15-js-Strings/strings.js
@@ -2,3 +2,12 @@ firstName = "Hasrat";
 lastName = "Afridi";
 fullName = firstName+" "+lastName;
 console.log(fullName)
+
+message = "h1"
+age = 3;
+
+console.log(message*age)
+
+yourAge = 10;
+yourMessage = "2";
+console.log(yourAge*yourMessage)
