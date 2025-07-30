e219dfe20d6c41cc9efea5a84d2f82534f7f7141
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-31 01:34:03 +0500

    Version 0.27

--- 16-js-Arrays-Operations/arrays-operations.js
+++ 16-js-Arrays-Operations/arrays-operations.js
@@ -5,4 +5,3 @@ console.log("Hobbies: "+hobbies)
 console.log("Hobby at index 0: "+hobbies.at(0))
 console.log("Operating system: "+operatingSystem)
 console.log("Combined two Arrays (Hobbies, Operating System): "+hobbies.concat(operatingSystem))
-
