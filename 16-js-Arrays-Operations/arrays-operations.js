cfb1fc6a669d8819d0e765fd2c015a45fdfa8552
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-30 12:33:31 +0500

    Version 0.27

---
+++ 16-js-Arrays-Operations/arrays-operations.js
@@ -0,0 +1,8 @@
+let hobbies = ["Video games","Ethical hacking","History"];
+let operatingSystem = ["Kali Linux"];
+
+console.log("Hobbies: "+hobbies)
+console.log("Hobby at index 0: "+hobbies.at(0))
+console.log("Operating system: "+operatingSystem)
+console.log("Combined two Arrays (Hobbies, Operating System): "+hobbies.concat(operatingSystem))
+
