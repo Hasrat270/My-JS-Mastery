357f33f5416e8364c6af79239a2a7316b792dde8
Hasrat Afridi <hasrat3701@gmail.com>
2025-07-30 12:16:08 +0500

    Version 0.26

--- 15-js-Strings/strings.js
+++ 15-js-Strings/strings.js
@@ -11,3 +11,13 @@ console.log(message*age)
 yourAge = 10;
 yourMessage = "2";
 console.log(yourAge*yourMessage)
+
+let userName = "Hasrat";
+console.log("Username length: "+userName.length);
+
+console.log(userName.charAt(0))
+console.log(userName.charAt(1))
+console.log(userName.charAt(2))
+console.log(userName.charAt(3))
+console.log(userName.charAt(4))
+console.log(userName.charAt(5))
