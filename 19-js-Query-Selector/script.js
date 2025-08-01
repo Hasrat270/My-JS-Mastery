3537c050f368a18a47f2fcad37be5e346c71a1b5
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-01 10:40:11 +0500

    Version 0.28

--- 19-js-Query-Selector/script.js
+++ 19-js-Query-Selector/script.js
@@ -1,4 +1,4 @@
 // Here we will use Query Selector
 
-let document.querySelector("#external-link").href = "https://www.hackerone.com";
-let document.querySelector("a").href = "https://www.hackerone.com";
+let linkHref = document.querySelector("#external-link");
+linkHref.href = "https://www.Youtube.com";
