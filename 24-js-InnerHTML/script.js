d25d88b5abc3bf1707f6d28f8f1edd6d0d97edc8
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-02 01:09:40 +0500

    Version 0.28

---
+++ 24-js-InnerHTML/script.js
@@ -0,0 +1,5 @@
+let headingFour = document.querySelector("h4");
+let headingFive = document.querySelector("h5");
+
+headingFour.innerHTML = "<p>Difference Between Text content and Inner HTML <span style="color: red">(InnerHTML)</span></p>";
+headingFive.textContent = "<p>Difference Between Text content and Inner HTML (TextContent)</p>";
