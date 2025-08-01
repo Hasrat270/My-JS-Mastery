ea213d234767c85adc8f3f627c9b7c4b2802cbda
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-01 10:57:05 +0500

    Version 0.28

--- 19-js-Query-Selector/script.js
+++ 19-js-Query-Selector/script.js
@@ -2,3 +2,6 @@
 
 let linkHref = document.querySelector("#external-link");
 linkHref.href = "https://www.Youtube.com";
+
+let mainHeading = document.querySelector("h1");
+mainHeading.innerHTML = "Good Bye World";
