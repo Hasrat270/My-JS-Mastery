0a176c54cf15cf16ed894a316a804f91dcf3a36e
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-03 01:12:34 +0500

    Version 0.29

---
+++ 28-js-Challenge/script.js
@@ -0,0 +1,9 @@
+const maxCharacters = 60;
+const input = document.getElementById('myInput');
+
+function updateRemainingCharacters() {
+    const remaining = maxCharacters - input.value.length;
+    document.getElementById('remaining').textContent = remaining;
+}
+
+input.addEventListener("input", updateRemainingCharacters);
