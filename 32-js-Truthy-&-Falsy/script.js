56e3e928a71756e965d2a27e3f649273e6864591
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-04 22:31:40 +0500

    Version 0.30

---
+++ 32-js-Truthy-&-Falsy/script.js
@@ -0,0 +1,8 @@
+// JavaScript convert the empty string into Zero "0" which is equal to false
+// that's why is shows that the input is valid
+
+const enteredUserName = "";
+
+if (enteredUserName) {
+    console.log("Username is Valid!")
+}
