d3e3d5cb52e2a7cbe6b31139a997c099e86b0dac
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-06 13:03:17 +0500

    Version 0.30

--- 33-js-Loops/script.js
+++ 33-js-Loops/script.js
@@ -2,13 +2,23 @@
 
 // 1. For Loop
 
-for (let i = 10; i <= 100; i++) {
+for (let i = 10; i <= 100; i += 10) {
     console.log(i);
 }
 
 // 2. For of Loop
 
+const users = [{ name: "Alice", age: 25 }, { name: "Bob", age: 30 }, { name: "Charlie", age: 35 }];
+for (let user of users) {
+    console.log(user.name.toUpperCase());
+}
+
 const fruits = ["apple", "banana", "orange", "grape"];
 for (let fruit of fruits) {
-    console.log(fruit);
+    console.log(fruit.toUpperCase());
+}
+
+for (let i = 0; i < fruits.length; i++) {
+    console.log(fruits[i]);
+
 }
