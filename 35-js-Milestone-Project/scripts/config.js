ae71af463b4bd690509c8362e733889ef1098f53
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-09 14:25:36 +0500

    Version 0.32

--- 35-js-Milestone-Project/scripts/config.js
+++ 35-js-Milestone-Project/scripts/config.js
@@ -10,6 +10,7 @@ function closePlayerConfig() {
     backdrop.style.display = 'none';
     formElement.firstElementChild.classList.remove('error');
     errorsOutputElement.textContent = '';
+    formElement.firstElementChild.lastElementChild.value = '';
 }
 
 function savePlayerConfig(event) {
@@ -27,9 +28,12 @@ function savePlayerConfig(event) {
     const updatedPlayerDataElement = document.getElementById('player-' + editedPlayer + '-data');
     updatedPlayerDataElement.children[1].textContent = enteredPlayerName; // change the name in the html
 
-    if (editedPlayer === 1) {
-        players[0].name = enteredPlayerName;
-    } else {
-        players[1].name = enteredPlayerName;
-    }
+    // if (editedPlayer === 1) {
+    //     players[0].name = enteredPlayerName;
+    // } else {
+    //     players[1].name = enteredPlayerName;
+    // }
+    players[editedPlayer - 1].name = enteredPlayerName; // editedPlayer - 1 === 0 || 1
+
+    closePlayerConfig();
 }
