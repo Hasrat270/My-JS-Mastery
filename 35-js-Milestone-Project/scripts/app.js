bf0145f3863519ebeceb8928e95f877657b861bc
Hasrat Afridi <hasrat3701@gmail.com>
2025-08-09 14:15:29 +0500

    Version 0.32

--- 35-js-Milestone-Project/scripts/app.js
+++ 35-js-Milestone-Project/scripts/app.js
@@ -1,11 +1,28 @@
-const playerConfigOverlay = document.getElementById('config-overlay');
-const backdrop = document.getElementById('backdrop');
+let editedPlayer = 0;
 
-const editPlayer1Btn = document.getElementById('edit-player-1');
-const editPlayer2Btn = document.getElementById('edit-player-2');
-const cancelConfigBtn = document.getElementById('cancel-config-btn');
+const players = [
+    { 
+        name: "", 
+        Symbol: "X" 
+    }, 
+    {
+        name: "",
+        Symbol: "O"
+    },
+];
 
-editPlayer1Btn.addEventListener("click" ,openPlayerConfig);
-editPlayer2Btn.addEventListener("click" ,openPlayerConfig);
-cancelConfigBtn.addEventListener("click" ,closePlayerConfig);
-backdrop.addEventListener("click" ,closePlayerConfig);
+const playerConfigOverlay = document.getElementById("config-overlay");
+const backdrop = document.getElementById("backdrop");
+const formElement = document.querySelector("form");
+const errorsOutputElement = document.getElementById("config-errors");
+
+const editPlayer1Btn = document.getElementById("edit-player-1");
+const editPlayer2Btn = document.getElementById("edit-player-2");
+const cancelConfigBtn = document.getElementById("cancel-config-btn");
+
+editPlayer1Btn.addEventListener("click", openPlayerConfig);
+editPlayer2Btn.addEventListener("click", openPlayerConfig);
+cancelConfigBtn.addEventListener("click", closePlayerConfig);
+backdrop.addEventListener("click", closePlayerConfig);
+
+formElement.addEventListener("submit", savePlayerConfig);
